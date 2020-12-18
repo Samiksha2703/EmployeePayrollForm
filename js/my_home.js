d968c23c2c6399887b2173f4c48d03c930fe3c01
samiksha Shende <shende.samiksha@rediffmail.com>
2020-12-18 22:40:43 +0530

    [Samiksha] ADD. employee payroll program files to get employee payroll details from JSON object.

--- js/my_home.js
+++ js/my_home.js
@@ -4,22 +4,59 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th><Department</th>" + "<th>Salary</th><th>Start Date</th><th>Action</th>";
+    let empPayrollData = createEmployeePayrollJSON()[0];
     const innerHtml = `${headerHtml}
     <tr>
-    <td><img class="profile" alt="" src="../assets/Ellipse -4.png">
-    </td>
-    <td>Samiksha Shende</td>
-    <td>Female</td>
+    <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+    <td>${empPayrollData._name}</td>
+    <td>${empPayrollData._gender}</td> 
+    <td>${getDeptHtml(empPayrollData._department)}</td>
+    <td>${empPayrollData._salary}</td>
+    <td>${empPayrollData._startDate}</td>
     <td>
-        <div class='dept-label'>HR</div>
-        <div class='dept-label'>Engineer</div>
-    <td>3000000</td>
-    <td>5th May 2017</td>
-    <td>
-        <img id="1" onclick="remove(this)" alt="delete" src="../assets/delete-black-18dp.svg">
-        <img id="1" onclick="update(this)" alt="edit" src="../assets/create-black-18dp.svg">
+        <img name="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/delete-black-18dp.svg">
+        <img name="${empPayrollData._id}" onclick="update(this)" alt="edit" src="../assets/create-black-18dp.svg">
     </td>
     </tr>
     `;
     document.querySelector('#table-display').innerHTML = innerHtml;
 }
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for (const dept of deptList) {
+        deptHtml = `${deptHtml} <dev class='dept-lable'>${dept}</div>`
+    }
+    return deptHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [
+        {
+            _name: 'Samiksha Shende',
+            _gender: 'female',
+            _department: [
+                'Engineering'
+            ],
+            _salary: '500000',
+            _startDate: '29 Oct 2019',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/Ellipse -7.png'
+        },
+        {
+            _name: 'Apurva Yede',
+            _gender: 'female',
+            _department: [
+                'Engineering', 'Finance'
+            ],
+            _salary: '500000',
+            _startDate: '29 Oct 2019',
+            _note: '',
+            _id: new Date().getTime() + 1,
+            _profilePic: '../assets/Ellipse -4.png'
+        }
+    ];
+    return empPayrollListLocal;
+}
+
