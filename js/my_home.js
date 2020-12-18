19ec075739cf890c31c40854c9c33be4a151d25a
samiksha Shende <shende.samiksha@rediffmail.com>
2020-12-18 23:15:04 +0530

    [Samiksha] ADD. employee payroll program files to display complete details data from JSON object using for loop.

--- js/my_home.js
+++ js/my_home.js
@@ -4,10 +4,13 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th><Department</th>" + "<th>Salary</th><th>Start Date</th><th>Action</th>";
-    let empPayrollData = createEmployeePayrollJSON()[0];
-    const innerHtml = `${headerHtml}
+    innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for (const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
     <tr>
     <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+    
     <td>${empPayrollData._name}</td>
     <td>${empPayrollData._gender}</td> 
     <td>${getDeptHtml(empPayrollData._department)}</td>
@@ -19,6 +22,7 @@ const createInnerHtml = () => {
     </td>
     </tr>
     `;
+    }
     document.querySelector('#table-display').innerHTML = innerHtml;
 }
 
